20999868660511b3e7f71442fe042f32771482e1
emreren <16957951+emreren@users.noreply.github.com>
2025-09-16 14:37:53 +0300

    refactor: reorganize project structure to Python standards

--- scripts/run_web.py
+++ scripts/run_web.py
@@ -2,7 +2,7 @@
 Run the FastAPI web server.
 """
 import uvicorn
-from securnote.web.app import app
+from securnote.web.main import app
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
